58e8c2f198951fb4889610ffa13d41575527753e
mykolas <test@test.com>
2025-01-18 22:53:54 +0200

    added email template maker

---
+++ Constants.cs
@@ -0,0 +1,113 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace draft_data
+{
+    public static class DomainConstants
+    {
+
+        public static string GetPage
+        (
+            int hasToProvideData,
+            int hasToProvideDataUntilExact,
+            int draftProcedureInProgress,
+            int isAsignedAndNeedsToArrive,
+            int quicklyHasToContactAndArrive,
+            int hasToAttendMedicalScreening,
+            int draftHasBeenPostponed
+        )
+        {
+            // string prevStateColorCode;
+            // if (prevState == "Online")
+            // {
+            //     prevStateColorCode = "rgb(56, 148, 54)";
+            // }
+            // else if (prevState == "NeedsAttention")
+            // {
+            //     prevStateColorCode = "rgb(197, 67, 68)";
+            // }
+            // else
+            // {
+            //     prevStateColorCode = "rgb(214, 186, 70)";
+            // }
+
+            // string currentStateColorCode;
+            // if (currentState == "Online")
+            // {
+            //     currentStateColorCode = "rgb(56, 148, 54)";
+            // }
+            // else if (currentState == "NeedsAttention")
+            // {
+            //     currentStateColorCode = "rgb(197, 67, 68)";
+            // }
+            // else
+            // {
+            //     currentStateColorCode = "rgb(214, 186, 70)";
+            // }
+
+
+            string htmlString = $@"
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta http-equiv=""content-type"" content=""text/html; charset=UTF-8"">
+                    <title>Sauktinis</title>
+                    <meta charset=""UTF-8"">
+                    <meta name=""viewport"" content=""width=device-width, initial-scale=1"">
+                    <link rel=""stylesheet"" href=""W3.CSS%20Template_files/w3.css"">
+                    <link rel=""stylesheet"" href=""W3.CSS%20Template_files/css.css"">
+                    <link rel=""stylesheet"" href=""W3.CSS%20Template_files/font-awesome.min.css"">
+                    <style>
+                    body, h1, h2, h3, h4, h5, h6 {{ font-family: ""Montserrat"", sans-serif; }}
+                    .w3-row-padding img {{ margin-bottom: 12px; }}
+                    .w3-sidebar {{ width: 120px; background: #222; }}
+                    #main {{ margin-left: 120px; }}
+                    @media only screen and (max-width: 600px) {{ 
+                        #main {{ margin-left: 0; }} 
+                    }}
+                    </style>
+                </head>
+                <body class=""w3-black"">
+                    <div class=""w3-row w3-center w3-padding-16 w3-section w3-light-grey"">
+                    <div class=""w3-quarter w3-section"">
+                        <span class=""w3-xlarge"">{hasToProvideData}</span><br>
+                        Privalo pateikti duomenis
+                    </div>
+                    <div class=""w3-quarter w3-section"">
+                        <span class=""w3-xlarge"">{hasToProvideDataUntilExact}</span><br>
+                        Privalo pateikti duomenis iki konkrecios datos
+                    </div>
+                    <div class=""w3-quarter w3-section"">
+                        <span class=""w3-xlarge"">{draftProcedureInProgress}</span><br>
+                        Saukimo proceduros vykdomos
+                    </div>
+                    <div class=""w3-quarter w3-section"">
+                        <span class=""w3-xlarge"">{isAsignedAndNeedsToArrive}</span><br>
+                        Privalo atvykti i kompektavimo skyriu
+                    </div>
+                    <div class=""w3-quarter w3-section"">
+                        <span class=""w3-xlarge"">{quicklyHasToContactAndArrive}</span><br>
+                        Privalo skubiai susiekti ir atvykti i nurodyta skyriu
+                    </div>
+                    <div class=""w3-quarter w3-section"">
+                        <span class=""w3-xlarge"">{hasToAttendMedicalScreening}</span><br>
+                        Privalo atvykti pasitikrinti sveikatos
+                    </div>
+                    <div class=""w3-quarter w3-section"">
+                        <span class=""w3-xlarge"">{draftHasBeenPostponed}</span><br>
+                        Privalomoji karo tarnyba atidėta
+                    </div>
+                    </div>
+                </body>
+                </html>";
+
+
+            return htmlString;
+
+        }
+
+    }
+}
